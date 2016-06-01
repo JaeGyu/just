c1006a939bf9dd5cda7c3035d8b18e98d9a83ba9
jpmlyr <jpmlyr@gmail.com>
2016-06-02 00:53:57 +0900

    람다 연습

--- just/src/main/java/java8/ex/Lambda_1.java
+++ just/src/main/java/java8/ex/Lambda_1.java
@@ -1,5 +1,24 @@
 package java8.ex;
 
 public class Lambda_1 {
-	
+
+	public static void main(String[] args) {
+
+		MyFunctionInterface fi;
+
+		fi = () -> {
+			String str = "method call1";
+			System.out.println(str);
+		};
+		fi.method();
+
+		fi = () -> {
+			System.out.println("method call2");
+		};
+		fi.method();
+
+		fi = () -> System.out.println("method call3");
+		fi.method();
+
+	}
 }
---
+++ just/src/main/java/java8/ex/MyFunctionInterface.java
@@ -0,0 +1,8 @@
+package java8.ex;
+
+@FunctionalInterface
+public interface MyFunctionInterface {
+
+	public void method();
+
+}

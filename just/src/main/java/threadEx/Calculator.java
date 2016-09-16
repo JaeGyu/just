c8939f51bd6c83298b9458c2a9893bdd4ef33d4e
jaegyu <jpmlyr@gmail.com>
2016-09-16 15:14:19 +0900

    .

---
+++ just/src/main/java/threadEx/Calculator.java
@@ -0,0 +1,20 @@
+package threadEx;
+
+public class Calculator {
+	private int memory;
+
+	public int getMemory() {
+		return this.memory;
+	}
+
+	public void setMemory(int memory) {
+		this.memory = memory;
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(Thread.currentThread().getName() + ":" + this.memory);
+	}
+}

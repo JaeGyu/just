df79cfc31d900c91bbec07abc5e01df6900cb24c
jaegyu <jpmlyr@gmail.com>
2016-09-25 10:11:09 +0900

    .

--- just/src/main/java/java8/ex/MyFunctionalInterfaceWithReturnExampleReview.java
+++ just/src/main/java/java8/ex/MyFunctionalInterfaceWithReturnExampleReview.java
@@ -11,5 +11,12 @@ public class MyFunctionalInterfaceWithReturnExampleReview {
 
 		fi = (x, y) -> x + y;
 		System.out.println(fi.method(2, 3));
+
+		fi = (x, y) -> sum(x, y);
+		System.out.println(fi.method(3, 4));
+	}
+
+	public static int sum(int x, int y) {
+		return x + y;
 	}
 }

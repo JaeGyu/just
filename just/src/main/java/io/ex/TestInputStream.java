570f3204ba16d6f849a8c36933f7d066aa152347
jaegyu <jpmlyr@gmail.com>
2016-10-05 10:35:54 +0900

    .

---
+++ just/src/main/java/io/ex/TestInputStream.java
@@ -0,0 +1,11 @@
+package io.ex;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class TestInputStream {
+	public static void main(String[] args) throws Exception {
+		InputStream in = new FileInputStream("\\dev\\javaEx\\zzz\\aaa.txt");
+		
+	}
+}

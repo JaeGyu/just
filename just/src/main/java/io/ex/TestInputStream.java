ea28745c435b851669f022985545874bbcd4ee33
jaegyu <jpmlyr@gmail.com>
2016-10-05 10:55:52 +0900

    .

--- just/src/main/java/io/ex/TestInputStream.java
+++ just/src/main/java/io/ex/TestInputStream.java
@@ -1,11 +1,34 @@
 package io.ex;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class TestInputStream {
-	public static void main(String[] args) throws Exception {
-		InputStream in = new FileInputStream("\\dev\\javaEx\\zzz\\aaa.txt");
-		
+	public static void main(String[] args) {
+		InputStream in = null;
+		try {
+			in = new FileInputStream("/Users/jaegyuhan/dev/javaEx/zzz/aaa.txt");
+			while (true) {
+				int data = in.read();
+				System.out.println(data);
+				if (data == -1) {
+					break;
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("파일이 존재 하지 않습니다.");
+		} catch (IOException e) {
+			System.out.println("IOException이 발생을 했습니다.");
+		} finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }

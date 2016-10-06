f66a4b15040df157f49cc3adb3615b3b97954dca
jaegyu <jpmlyr@gmail.com>
2016-10-06 19:52:28 +0900

    .

--- just/src/main/java/io/ex/TestInputStream.java
+++ just/src/main/java/io/ex/TestInputStream.java
@@ -15,6 +15,13 @@ public class TestInputStream {
 				//read()는 1byte를 읽어 드린다.
 				int data = in.read();
 				System.out.println(data);
+				
+				/*
+				 * read()시 한바이트를 읽어오게 되면 -127~127 사이의 값을 대상으로 하지만
+				 * 리턴되는 타입은 int이다. 이는 read()내부적으로 -127~127사이의 값에 255를 더해서
+				 * 어떠한 경우에도 양수의 값을 리턴하기위함이다. 
+				 * 따라서 -1이 나오면 더이상 읽을 값이 없다는 뜻이다. EOF
+				 * */
 				if (data == -1) {
 					break;
 				}

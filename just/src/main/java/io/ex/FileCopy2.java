c8a6f7390cd026dda9a474aff226f059c43ef433
jaegyu <jpmlyr@gmail.com>
2016-10-07 21:19:02 +0900

    .

---
+++ just/src/main/java/io/ex/FileCopy2.java
@@ -0,0 +1,38 @@
+package io.ex;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileCopy2 {
+	public static void main(String[] args) {
+		InputStream in = null;
+		OutputStream out = null;
+
+		try {
+			in = new FileInputStream("/Users/jaegyuhan/dev/javaEx/zzz/bbb.txt");
+			out = new FileOutputStream("ccc.txt");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (out != null) {
+				try {
+					out.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}

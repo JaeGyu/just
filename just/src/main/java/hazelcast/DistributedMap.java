c42b410a68cb0dd3d9258b34580da8ee87dcc90c
jaegyu <jpmlyr@gmail.com>
2016-11-06 18:20:29 +0900

    HazelCast 연

---
+++ just/src/main/java/hazelcast/DistributedMap.java
@@ -0,0 +1,24 @@
+package hazelcast;
+
+import java.util.concurrent.ConcurrentMap;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+public class DistributedMap {
+	public static void main(String[] args) {
+		Config config = new Config();
+
+		HazelcastInstance h = Hazelcast.newHazelcastInstance(config);
+
+		ConcurrentMap<String, String> map = h.getMap("my-distributed-map");
+
+		map.put("key", "value");
+		map.get("key");
+
+		map.putIfAbsent("somekey", "somevalue");
+		map.replace("key", "value", "newvalue");
+
+	}
+}

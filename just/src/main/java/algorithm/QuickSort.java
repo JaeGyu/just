120c9b38e2d868792af07fbd569af98c855146e4
icpu <icpu@naver.com>
2017-03-26 14:56:21 +0900

    퀵소트 다시 구현

--- just/src/main/java/algorithm/QuickSort.java
+++ just/src/main/java/algorithm/QuickSort.java
@@ -1,34 +1,46 @@
 package algorithm;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
+/*
+ * void quick([],lo,hi)
+ * index patition([],lo,hi)
+ * swap([], sourceIdx, targetIdx)
+ * */
 public class QuickSort {
 	public static void main(String[] args) {
 		Integer[] nums = { 2, 3, 1, 5, 6, 4 };
-		quick(nums);
+		quick(nums, 0, nums.length - 1);
 		System.out.println(Arrays.toString(nums));
 	}
 
-	public static void quick(Integer[] nums) {
-		if (nums.length == 0)
-			return;
+	public static void quick(Integer[] nums, int lo, int hi) {
+		if (lo < hi) {
+			int p = patition(nums, lo, hi);
+			quick(nums, lo, p - 1);
+			quick(nums, p + 1, hi);
+		}
+	}
 
-		int pivot = nums[0];
+	private static int patition(Integer[] nums, int lo, int hi) {
+		int pivotNum = nums[hi];
+		int currentIdx = lo;
 
-		List<Integer> lP = new ArrayList<>();
-		List<Integer> rP = new ArrayList<>();
-
-		for (int i = 1; i < nums.length; i++) {
-			if (pivot > nums[i]) {
-				lP.add(nums[i]);
-			} else {
-				rP.add(nums[i]);
+		for (int i = lo; i < hi; i++) {
+			if (nums[i] < pivotNum) {
+				swap(nums, i, currentIdx); // 맨 왼쪽부터 피봇 보다 작은 값들을 쌓는다.
+				currentIdx++;
 			}
 		}
 
-		quick(lP.toArray(new Integer[lP.size()]));
-		quick(rP.toArray(new Integer[lP.size()]));
+		swap(nums, currentIdx, hi); // 피봇과 피봇보다 작은값의 경계 선과 교환 해준다.
+		return currentIdx;
 	}
+
+	private static void swap(Integer[] nums, int sourceIdx, int targetIdx) {
+		int tmp = nums[sourceIdx];
+		nums[sourceIdx] = nums[targetIdx];
+		nums[targetIdx] = tmp;
+	}
+
 }
